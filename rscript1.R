b79164a73ce677845325452daa6a36c32ef02931
swoldetsadick <swoldetsadick@ineteconomics.org>
2014-10-23 22:34:32 +0200

    messages

--- rscript1.R
+++ rscript1.R
@@ -30,3 +30,24 @@ m <- m + annotate("text", label = "Theoretical", x = 7, y = .1, size = 4, colour
 m <- m + annotate('text', label =  paste(paste(paste("E(","bar(X)", sep=""),"):", sep=""),xbar,sep=" "),parse=T, x = 5.75, y = .6, size = 8, colour = "red")
 m <- m + annotate('text', label =  paste(paste(paste("mu", ": 1/", sep=""), "lambda", sep=""), " : 5", sep =""),parse=T, x = 4.25, y = .65, size = 8, colour = "blue")
 m
+#Calculating averages by row, or for the 1000 different simulations over the 40 sample drawn
+Ssqr <- round((sd(apply(data, 1, mean))), digits = 2)
+#Calculating theoretical mean
+mu <- round(((1/lambda)/n), digits = 2)
+
+lambdavals <- seq(.1, .4, by = 0.01)
+nosim1 <- 10
+n1 <- 10
+
+coverage <- sapply(lambdavals, function(lambd) {
+        set.seed(123)
+        mu_hats   <- apply(matrix(rexp(n1*nosim1, rate = lambd), nosim1, n1), 1, mean)
+        theta_hat <- sd(apply(matrix(rexp(n1*nosim1, rate = lambd), nosim1, n1), 1, mean))
+        ll <- mu_hats - qnorm(0.975) * theta_hat
+        ul <- mu_hats + qnorm(0.975) * theta_hat
+        mean(ll < (1/lambd) & ul > (1/lambd))
+        })
+
+# Plot coverage
+library(ggplot2)
+qplot(lambdavals, coverage) + geom_hline(yintercept=0.95, col=2)
---
+++ rscript2.R
@@ -0,0 +1,25 @@
+library(datasets)
+data(ToothGrowth)
+data <- ToothGrowth
+names(data)
+sum(is.na(data))
+dim(data)
+data$dose <- ifelse(data$dose == .5, "low", ifelse(data$dose == 1, "med", "hi"))
+for(i in 1:3){if(i!=1) {data[,i] <- as.factor(data[,i])} else {data[,i] <- as.numeric(data[,i])}}
+str(data)
+
+attach(data)
+library(Hmisc)
+mytable <- table(supp, dose, cut2(len, g = 4))
+ftable(mytable, dnn = c("Supplement type", "Dose in milligrams", "Tooth length"))
+
+
+m = ftable(supp, dose, cut2(len, g = 4), row.vars=1:2) # 3-way table of group means
+names(attr(m, "row.vars")) = c("Supplement Type","Dose") # Add names for the row variables
+names(attr(m, "col.vars")) = "Tooth Length" # ...and the column variable
+write.ftable(m, file="", digits=3, quote=FALSE) # Pretty-print the table to a file
+
+
+tapply(len, list(supp, dose), mean)
+library(nlme)
+isBalanced(data, countOnly = TRUE) 
